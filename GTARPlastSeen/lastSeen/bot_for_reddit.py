87bf2da50fe8f0054cef2dfcb4154518e549678d
Sean_J <SeanJan10@hotmail.com>
2022-10-01 01:47:53 -0400

    reddit bot now responds to when user mentions it to display the last five appearances of a character

--- GTARPlastSeen/lastSeen/bot_for_reddit.py
+++ GTARPlastSeen/lastSeen/bot_for_reddit.py
@@ -17,11 +17,40 @@ from django.shortcuts import get_object_or_404
 #global vars
 redditUsername = config('redditUsername')
 endString = f"\n\n*This action was automated by a bot. It has no affiliation to the Staff or Owner of NoPixel. You can contact the owner of this bot [here.](https://www.reddit.com/user/{redditUsername})*"
+myURL = "http://127.0.0.1:8000/lastSeenRP/"
+
 
 def redditResponseToRecentAppearances(data, mention):
     data.pop(0)
     print("you selected to list the last 5 appearances of a character")
+    if len(data) == 2:
+        fName = data[0]
+        lName = data[1]
+        try:
+            character = rpCharacter.objects.get(character_first_name=fName, character_last_name=lName)
+        except ObjectDoesNotExist:
+            mention.reply(body=f"ERROR: The character you entered does not exist. If this character does exist then they have not been entered into the database. This can be done at {myURL}{endString}")
+            mention.mark_read()
+            return
+        characterAppearances = Appearance.objects.filter(character_name=character)
+        latestAppearances = characterAppearances.order_by('-date_of_appearance')[:]
+        strAppearance = ''
+        if not latestAppearances:
+            mention.reply(body=f"{fName} {character.character_nick_name} {lName} has no appearances saved in the database. {endString}")
+            mention.mark_read()
+        else:
+            for appearance in latestAppearances:
+                appearTime = appearance.date_of_appearance.strftime("%b. %d, %Y, %H:%M")
+                pubTime = appearance.publish_time.strftime("%b. %d, %Y, %H:%M")
+                strAppearance = strAppearance + f"* appeared on {appearTime} on [{appearance.clip_Streamer}]({appearance.twitch_clip_URL})'s stream. Updated at {pubTime}\n\n"
 
+        reply_template = f"Recent appearances for {fName} {character.character_nick_name} {lName}\n\n{strAppearance}{endString}"  
+        mention.reply(body=reply_template)
+        mention.mark_read() 
+    else:
+        mention.reply(body=f"ERROR: Incorrect Number of arguments. Please only provide the first and last name of the character. (e.g. /u/NoPixelAppearanceBot create_appearance Avon Barksdale).\nNote: if the character has more than two names, then include the additional names in the last name and indicate the additional names with underscores in place of spaces. (e.g. /u/NoPixelAppearanceBot create_appearance Arush Patel_Santana{endString}")
+        mention.mark_read()
+        
 
 
 
@@ -36,7 +65,7 @@ def redditResponseToCreateAppearance(data, parentPost, mention):
         #check if whitelisted url
         if validClipURL(clipURL) == False:
             mention.reply(body=f'ERROR: supported clip URLS include twitch, youtube, facebook and streamable.{endString}')
-            mention.save()
+            mention.mark_read()
             return
 
         #should not need to check appearance as reddit posts cannot be posted in the future
@@ -47,27 +76,27 @@ def redditResponseToCreateAppearance(data, parentPost, mention):
         characterFName = data[0]
         characterLName = data[1]
         clipStreamerName = data[2]
-        print(f'Created a new appearance for: First name = {characterFName}, Last Name = {characterLName}, Streamer = {clipStreamerName}')
+        #print(f'Created a new appearance for: First name = {characterFName}, Last Name = {characterLName}, Streamer = {clipStreamerName}')
 
         try:
-            appearance = createAppearance(characterFName, characterLName, clipURL, appearanceTime, clipStreamerName, now)
+            character = rpCharacter.objects.get(character_first_name=firstName, character_last_name=lastName)
+            appearance = createAppearance(character, clipURL, appearanceTime, clipStreamerName, now)
         except ObjectDoesNotExist:
             #print("ERROR: The character you entered does not exist. If this character does exist then they have not been entered into the database. This can be done at www.url.com")
-            mention.reply(body=f"ERROR: The character you entered does not exist. If this character does exist then they have not been entered into the database. This can be done at www.url.com{endString}")
-            mention.save()
+            mention.reply(body=f"ERROR: The character you entered does not exist. If this character does exist then they have not been entered into the database. This can be done at {myURL}{endString}")
+            mention.mark_read()
             return
-        reply_template = f"New appearance created for {characterFName} {characterLName}! View other appearances by this character [HERE.](http://127.0.0.1:8000/lastSeenRP/character/{characterFName}_{characterLName}/){endString}"
+        reply_template = f"New appearance created for {characterFName} {character.character_nick_name} {characterLName}! View other appearances by this character [HERE.](http://127.0.0.1:8000/lastSeenRP/character/{characterFName}_{characterLName}/){endString}"
         mention.reply(body=reply_template)
-        mention.save()
+        mention.mark_read()
     else:
         #print("ERROR: Incorrect Number of arguments. Please only provide the first and last name of the character, and the name of the channel where you clipped from. (e.g. /u/NoPixelAppearanceBot create_appearance Avon Barksdale LIRIK).\nNote: if the character has more than two names, then include the additional names in the last name and indicate the additional names with underscores in place of spaces. (e.g. /u/NoPixelAppearanceBot create_appearance Arush Patel_Santana SayeedBlack")
         mention.reply(body=f"ERROR: Incorrect Number of arguments. Please only provide the first and last name of the character, and the name of the channel where you clipped from. (e.g. /u/NoPixelAppearanceBot create_appearance Avon Barksdale LIRIK).\nNote: if the character has more than two names, then include the additional names in the last name and indicate the additional names with underscores in place of spaces. (e.g. /u/NoPixelAppearanceBot create_appearance Arush Patel_Santana SayeedBlack{endString}")
-        mention.save()
+        mention.mark_read()
     
 
 #function to create a valid appearance
-def createAppearance(firstName, lastName, clipURL, appearanceTime, clipStreamerName, publishTime):
-    character = rpCharacter.objects.get(character_first_name=firstName, character_last_name=lastName)
+def createAppearance(character, clipURL, appearanceTime, clipStreamerName, publishTime):
     newAppeareance = Appearance(
         character_name=character, 
             twitch_clip_URL=clipURL, 
@@ -99,7 +128,7 @@ def configReddit():
 def main():
     reddit = configReddit()
     for mention in reddit.inbox.mentions(limit=30):
-        time.sleep(15)
+        time.sleep(3)
         print(mention.new)
         if mention.new == True:
             if (mention.subreddit == 'test' or mention.subreddit == 'RPClipsGTA'):
