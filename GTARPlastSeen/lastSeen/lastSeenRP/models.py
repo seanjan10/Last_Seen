4a8546aac2cdaad852971aab35422e8c1bb9f41b
Sean_J <SeanJan10@hotmail.com>
2022-09-30 21:24:46 -0400

    modified reddit bot and fixed searching for multiple names

--- GTARPlastSeen/lastSeen/lastSeenRP/models.py
+++ GTARPlastSeen/lastSeen/lastSeenRP/models.py
@@ -39,17 +39,13 @@ class rpCharacter(models.Model):
         super().save()
 
 class Appearance(models.Model):
-    #future fields to be added
-    #adjust twitch_clip_URL to include streamable, youtube, facebook etc. 
-    #link to the clip streamers twitch/youtube/facebook
-    #change times to only include UTC time
-    #username of reddit user etc who submitted the appearance
     character_name = models.ForeignKey(rpCharacter, on_delete=models.CASCADE)
     #twitch_clip_URL = models.CharField(max_length=100)
     twitch_clip_URL = models.URLField(max_length=100)
     date_of_appearance = models.DateTimeField('date and time that the character showed up')
     clip_Streamer = models.CharField(max_length=50) #in case the clip gets deleted they can find the streamers vod and watch
     publish_time = models.DateTimeField('date and time a user submitted this post', default= pytz.UTC.localize(datetime.now()))
+    submittedBy = models.CharField(max_length=25, default="")
     def __str__(self):
         return self.twitch_clip_URL + ', ' + self.date_of_appearance.strftime("%Y-%m-%d, %H:%M:%S") + ', ' + self.clip_Streamer + ', ' + self.publish_time.strftime("%m/%d/%Y, %H:%M:%S")
     #should change to recently appeared, or make a new func
