d415537486bd1ad9122e271ead7442b463b655b9
Sean_J <SeanJan10@hotmail.com>
2022-10-15 23:06:46 -0400

    removed datetime.now for timezone.now

--- GTARPlastSeen/lastSeen/lastSeenRP/tests.py
+++ GTARPlastSeen/lastSeen/lastSeenRP/tests.py
@@ -12,14 +12,14 @@ class AppearanceModelTests(TestCase):
     def test_appearance_is_before_right_now(self):
         ''' form should not accept appearances of a character that appear in the future '''
 
-        time = datetime.now() + timedelta(days=30)
+        time = timezone.now() + timedelta(days=30)
         future_appearance = Appearance(date_of_appearance=time)
         self.assertIs(future_appearance.recently_published(), False)
 
     def test_was_published_recently_with_old_appearance(self):
         '''was_published_recently() returns False for appearances that were published more than a day ago'''
 
-        time = datetime.now() - timedelta(days=1, hours=1)
+        time = timezone.now() - timedelta(days=1, hours=1)
         old_appearance = Appearance(date_of_appearance=time)
         self.assertIs(old_appearance.recently_published(), False)
 
@@ -27,7 +27,7 @@ class AppearanceModelTests(TestCase):
     def test_was_published_recently_with_recent_appearance(self):
         '''was_published_recently() returns True for appearances that were published within the last day'''
 
-        time = datetime.now()
+        time = timezone.now()
         recent_appearance = Appearance(date_of_appearance=time)
         self.assertIs(recent_appearance.recently_published(), True)
 
@@ -35,7 +35,7 @@ def create_character(char_fname, char_lname, char_play_by):
     return rpCharacter.objects.create(character_first_name=char_fname, character_last_name=char_lname, character_played_by=char_play_by)
 
 def create_appearance(character, twitch_clip_URL, days, clip_Streamer, publish_time):
-    time = datetime.now() + timedelta(days=days)
+    time = timezone.now() + timedelta(days=days)
     return Appearance.objects.create(character_name=character, twitch_clip_URL=twitch_clip_URL, date_of_appearance=time, clip_Streamer=clip_Streamer, publish_time=publish_time)
 
 class AppearanceViewTests(TestCase):
