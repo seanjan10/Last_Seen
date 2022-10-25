c210573b7272cb2e237e1e35489f6f317959b75c
Sean_J <SeanJan10@hotmail.com>
2022-10-24 22:04:48 -0400

    switched to wildcard in allowed-hosts

--- GTARPlastSeen/lastSeen/lastSeen/settings.py
+++ GTARPlastSeen/lastSeen/lastSeen/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://last-seen-in-gta-rp.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
