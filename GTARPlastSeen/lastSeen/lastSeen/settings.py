61b03630d74bd13ebd59a78b24c200cd4d588b6e
Sean_J <SeanJan10@hotmail.com>
2022-10-25 16:33:28 -0400

    adjusted settings allowed hosts, readded procfile with gunicorn

--- GTARPlastSeen/lastSeen/lastSeen/settings.py
+++ GTARPlastSeen/lastSeen/lastSeen/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', 'last-seen-in-gta-rp.herokuapp.com']
 
 
 # Application definition
