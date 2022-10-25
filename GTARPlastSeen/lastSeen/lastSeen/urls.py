eb3d53887fbc26fc47c2a8145303726a7306e96d
Sean_J <SeanJan10@hotmail.com>
2022-10-25 17:58:44 -0400

    testing removal of lastSeenRP from urls

--- GTARPlastSeen/lastSeen/lastSeen/urls.py
+++ GTARPlastSeen/lastSeen/lastSeen/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('lastSeenRP/', include('lastSeenRP.urls')),
+    path('', include('lastSeenRP.urls')),
     path('admin/', admin.site.urls),
 ]
 
